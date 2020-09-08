cd40d707c17ed52102596eaf3294ff59a7ede284
fredtavares2018 <nordesttino@hotmail.com>
2020-09-08 00:44:52 -0300

    variaveis

---
+++ estudoVariaveis.py
@@ -0,0 +1,35 @@
+# tipos de variáveis
+
+a = 2
+b = 3.4
+c = True
+d = 8 + 7j
+e = 'Teste'
+
+# Para saber os tipos usamos  type
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+# resposta do terminal
+
+# <class 'int'> inteiro
+# <class 'float'> decimal
+# <class 'bool'> boleano
+# <class 'complex'> complexo
+# <class 'str'> string
+
+# com elas podemos fazer as operações aritméticas 
+
+print('Soma ', a+b, 'Subtração ', a-b)
+print('Multiplicação ', a*b, 'Divisão ', a/b)
+print('Potenciação ', b**a)
+
+# resposta do terminal
+
+# Soma  5.4 Subtração  -1.4
+# Multiplicação  6.8 Divisão  0.5882352941176471
+# Potenciação  11.559999999999999
